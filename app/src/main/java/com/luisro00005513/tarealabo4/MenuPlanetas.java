fadc17e804527f0e165df06addcb058ef47a59b3
luisro369 <00005513@uca.edu.sv>
2018-04-12 22:28:06 -0600

    bug: cuando se le da click al linear layout se cierra la aplicacion

--- app/src/main/java/com/luisro00005513/tarealabo4/MenuPlanetas.java
+++ app/src/main/java/com/luisro00005513/tarealabo4/MenuPlanetas.java
@@ -1,11 +1,13 @@
 package com.luisro00005513.tarealabo4;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 /**
@@ -24,6 +26,19 @@ public class MenuPlanetas extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu_planetas, container, false);
-    }
+
+    }// on create
+
+
+
+
+
+    //metodo de fragmento el cual quisiera que llamara a la Main2Activity cada vez que se le de clic al LinearLayout
+    public void onClickLayout(View view){
+        Intent planeta = new Intent(getActivity().getApplicationContext(), Main2Activity.class);
+        startActivity(planeta);
+
+    }//onClickLayout
+
 
 }
